9c6ed435130293702d45d1ac0b683aa7a81aa84e
kuviman <kuviman@gmail.com>
2017-05-14 15:07:14 +0400

    JavaScript minification

--- build.rs
+++ build.rs
@@ -8,10 +8,21 @@ fn main() {
 
     Command::new("tsc.cmd")
         .arg("--outFile")
-        .arg(Path::new(&out_dir).join("codevisual-lib.js"))
+        .arg(Path::new(&out_dir).join("codevisual-lib-full.js"))
         .current_dir(Path::new("src").join("js"))
         .status()
         .expect("Could not compile TypeScript");
+    let js = Command::new("java")
+        .arg("-jar")
+        .arg("C:\\Programs\\bin\\closure-compiler.jar")
+        .arg(Path::new(&out_dir).join("codevisual-lib-full.js"))
+        .output()
+        .expect("Could not minify JavaScript")
+        .stdout;
+    File::create(Path::new(&out_dir).join("codevisual-lib.js"))
+        .expect("Could not create js file")
+        .write_all(&js)
+        .expect("Could not write js");
 
     let css = Command::new("lessc.cmd")
         .arg("--clean-css")
