78fba75b279ab338b7e8e77eb8d44bd28b285bf0
kuviman <kuviman@gmail.com>
2017-06-30 19:14:19 +0400

    Moved into_canvas_pos to html5 module

--- src/bindings/emscripten/src/html5/mod.rs
+++ src/bindings/emscripten/src/html5/mod.rs
@@ -9,3 +9,18 @@ pub use self::touch::*;
 
 mod webgl;
 pub use self::webgl::*;
+
+use ::*;
+
+pub(crate) fn into_canvas_pos(x: c_long, y: c_long) -> (f64, f64) {
+    let (x, y) = (x as f64, y as f64);
+    let (css_width, css_height) = unsafe {
+        let mut css_width: c_double = std::mem::uninitialized();
+        let mut css_height: c_double = std::mem::uninitialized();
+        emscripten_get_element_css_size(std::ptr::null(), &mut css_width, &mut css_height);
+        (css_width as f64, css_height as f64)
+    };
+    let (width, height) = get_canvas_size();
+    let (width, height) = (width as f64, height as f64);
+    (x * width / css_width, y * height / css_height)
+}
--- src/bindings/emscripten/src/lib.rs
+++ src/bindings/emscripten/src/lib.rs
@@ -23,16 +23,3 @@ pub fn get_proc_address(name: &str) -> *const c_void {
     let name = CString::new(name).expect("Could not convert name to C string");
     unsafe { ::emscripten_sys::emscripten_GetProcAddress(name.as_ptr()) as *const _ }
 }
-
-fn into_canvas_pos(x: c_long, y: c_long) -> (f64, f64) {
-    let (x, y) = (x as f64, y as f64);
-    let (css_width, css_height) = unsafe {
-        let mut css_width: c_double = std::mem::uninitialized();
-        let mut css_height: c_double = std::mem::uninitialized();
-        emscripten_get_element_css_size(std::ptr::null(), &mut css_width, &mut css_height);
-        (css_width as f64, css_height as f64)
-    };
-    let (width, height) = get_canvas_size();
-    let (width, height) = (width as f64, height as f64);
-    (x * width / css_width, y * height / css_height)
-}
