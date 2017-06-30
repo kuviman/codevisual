bdf6740e92d8604e4e09a053c3c100dd1279bf22
kuviman <kuviman@gmail.com>
2017-06-30 19:24:10 +0400

    Custom error in emscripten bindings

--- src/bindings/emscripten/src/html5/webgl.rs
+++ src/bindings/emscripten/src/html5/webgl.rs
@@ -1,12 +1,27 @@
 use ::*;
 
-pub fn create_gl_context() -> Result<(), String> {
+#[derive(Debug)]
+pub struct GLContextCreationError;
+
+impl std::error::Error for GLContextCreationError {
+    fn description(&self) -> &str {
+        "WebGL context could not be created"
+    }
+}
+
+impl std::fmt::Display for GLContextCreationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", std::error::Error::description(self))
+    }
+}
+
+pub fn create_gl_context() -> Result<(), GLContextCreationError> {
     unsafe {
         let mut attributes: EmscriptenWebGLContextAttributes = std::mem::uninitialized();
         emscripten_webgl_init_context_attributes(&mut attributes);
         let context = emscripten_webgl_create_context(std::ptr::null(), &attributes);
         if context <= 0 {
-            return Err(String::from("Could not create WebGL context"));
+            return Err(GLContextCreationError);
         }
         emscripten_webgl_make_context_current(context);
     }
