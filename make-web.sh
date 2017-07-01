9469cdfb196a967b6f217d01e5dac0d9f55409ab
kuviman <kuviman@gmail.com>
2017-07-01 13:21:58 +0400

    WebAssembly

--- make-web.sh
+++ make-web.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 config=release
 if [ -n "$1" ]; then
     config=$1
 fi
 
-cargoTarget=asmjs-unknown-emscripten
+target=wasm32 #asmjs
+cargoTarget=$target-unknown-emscripten
 cargoArgs=
 if [ "$config" == "release" ]; then
     cargoArgs=--release
@@ -16,3 +19,4 @@ PATH="$EMSCRIPTEN:$PATH"
 
 cargo build $cargoArgs --target=$cargoTarget --example playground
 cp target/$cargoTarget/$config/examples/playground.js examples/public/code.js
+cp target/$cargoTarget/$config/examples/*.wasm examples/public
