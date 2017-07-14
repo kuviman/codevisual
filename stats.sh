e01946edc08fd2e90813d9d74df5c7f12cd141fc
kuviman <kuviman@gmail.com>
2017-07-14 22:48:45 +0400

    Fixed stats script

--- stats.sh
+++ stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find playground src "(" -name "*.rs" -o -name "*.ts" -o -name "*.less" -o -name "*.pug" -o -name "*.html" ")" "!" -name "index.d.ts" | xargs wc -l -w
+find examples src "(" -name "*.rs" -o -name "*.ts" -o -name "*.less" -o -name "*.pug" -o -name "*.html" ")" "!" -name "index.d.ts" | xargs wc -l -w
