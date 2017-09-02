cf6af90df98035424bdee491f620f59c54244774
kuviman <kuviman@gmail.com>
2017-09-02 21:40:07 +0400

    Gaussian function for shaders

--- codewars2017/src/shader.glsl
+++ codewars2017/src/shader.glsl
@@ -15,8 +15,29 @@ uniform sampler2D texture;
 const vec3 PLAIN_COLOR = vec3(1.0, 1.0, 1.0);
 const vec3 FOREST_COLOR = vec3(0.0, 1.0, 0.0);
 const vec3 SWAMP_COLOR = vec3(0.1, 0.7, 0.5);
+
+vec4 blurred(sampler2D texture, vec2 pos) {
+    vec4 result = vec4(0.0);
+    float sum = 0.0;
+    const int OFF = 10;
+    for (int i = -OFF; i <= OFF; i++)
+        for (int j = -OFF; j <= OFF; j++) {
+            float g = G(vec2(i, j), 1.0);
+            sum += g;
+            result += texture2D(texture, pos + vec2(i, j) / 64.0) * g;
+        }
+    return result / sum;
+}
+
 void main() {
-    vec3 typ = texture2D(texture, pos).xyz;
+//    gl_FragColor = vec4(G(pos * 2.0 - 1.0, 0.5), 0.0, 0.0, 1.0);
+//    return;
+    vec3 typ = blurred(texture, pos).xyz;
+    if (typ.x > typ.y) {
+        typ = typ.x > typ.z ? vec3(1.0, 0.0, 0.0) : vec3(0.0, 0.0, 1.0);
+    } else {
+        typ = typ.y > typ.z ? vec3(0.0, 1.0, 0.0) : vec3(0.0, 0.0, 1.0);
+    }
     gl_FragColor = vec4(PLAIN_COLOR * typ.x + FOREST_COLOR * typ.y + SWAMP_COLOR * typ.z, 1.0);
 }
 #endif
--- src/core/material/src/glsl/prelude.glsl
+++ src/core/material/src/glsl/prelude.glsl
@@ -3,3 +3,8 @@ precision highp float;
 #endif
 
 #define PI 3.1415926535897932384626433832795
+
+float G(vec2 pos, float s) {
+    float sq2 = 2 * s * s;
+    return exp(-dot(pos, pos) / sq2) / (PI * sq2);
+}
