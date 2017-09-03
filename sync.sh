d860e9162d95e8d225d1f12a22563bda228a5884
kuviman <kuviman@gmail.com>
2017-09-03 21:25:19 +0400

    Sync CodeWars2017

--- sync.sh
+++ sync.sh
@@ -4,5 +4,8 @@ set -e
 
 ./make-web.sh asmjs
 ./make-web.sh wasm32
+pushd codewars2017
+./make-web.sh
+popd
 # ./make-windows.sh
 rsync -avz --delete target/web/* pi@pi.kuviman.com:/home/pi/codevisual/
