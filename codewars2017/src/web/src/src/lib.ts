b9942a750da1f25065e2b5a8008744be86a0c9d1
kuviman <kuviman@gmail.com>
2017-09-05 02:16:47 +0400

    Simplified stream_download

--- codewars2017/src/web/src/src/lib.ts
+++ codewars2017/src/web/src/src/lib.ts
@@ -2,79 +2,56 @@ namespace CodeWars {
     export function stream_download(path: string, callback: (addr: number) => void) {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', path);
+        xhr.addEventListener("error", function (e) {
+            console.error("error: " + e);
+        });
+        xhr.send();
+
+        const MAX_BYTES_AT_ONCE = 10 * 1024;
         let responsePos = 0;
         let line_start = 0;
         let finished = false;
-        let lines: string[] = [];
-
-        const MAX_LINES_AT_ONCE = 3;
-        const MAX_BYTES_AT_ONCE = 10 * 1024;
         let text = "";
 
-        function checkXHR() {
+        let buf_addr = 0;
+        let buf_len = 0;
+        function sendLine(line: string) {
+            let Module = (window as any).Module;
+            if (line.length + 1 > buf_len) {
+                if (buf_len != 0) {
+                    Module._free(buf_addr);
+                }
+                buf_len = line.length + 1;
+                buf_addr = Module._malloc(buf_len);
+            }
+            Module.writeAsciiToMemory(line, buf_addr);
+            callback(buf_addr);
+        }
+
+        function update() {
             if (xhr.readyState == 3 || xhr.readyState == 4) {
-                let checkedCur = 0;
-                while (responsePos < text.length && checkedCur < MAX_BYTES_AT_ONCE) {
+                let maxPos = Math.min(text.length, responsePos + MAX_BYTES_AT_ONCE);
+                while (responsePos < maxPos) {
                     if (text[responsePos++] == '\n') {
-                        lines.push(text.substring(line_start, responsePos));
+                        sendLine(text.substring(line_start, responsePos));
                         line_start = responsePos;
                     }
-                    checkedCur++;
                 }
                 if (responsePos == text.length) {
                     text = xhr.responseText;
                     if (responsePos == text.length && xhr.readyState == 4) {
                         if (line_start < text.length) {
-                            lines.push(text.substring(line_start));
+                            sendLine(text.substring(line_start));
                         }
                         finished = true;
                     }
                 }
             }
             if (!finished) {
-                setTimeout(checkXHR, 0);
+                setTimeout(update, 0);
             }
         }
 
-        xhr.addEventListener("error", function (e) {
-            console.error("error: " + e);
-        });
-        xhr.send();
-        let lastLine = 0;
-
-        let buf_addr: number = 0;
-        let buf_len: number = 0;
-
-        function update() {
-            if (lines.length - lastLine >= MAX_LINES_AT_ONCE || finished) {
-                let updated: number = 0;
-                while (lastLine < lines.length && updated < MAX_LINES_AT_ONCE) {
-                    let line = lines[lastLine];
-                    let Module = (window as any).Module;
-                    if (line.length + 1 > buf_len) {
-                        if (buf_len != 0) {
-                            Module._free(buf_addr);
-                        }
-                        buf_len = line.length + 1;
-                        buf_addr = Module._malloc(buf_len);
-                    }
-                    Module.writeAsciiToMemory(line, buf_addr);
-                    callback(buf_addr);
-                    updated++;
-                    lines[lastLine++] = undefined;
-                }
-            }
-            if (!finished || lastLine < lines.length) {
-                setTimeout(update, (finished || lines.length - lastLine >= MAX_LINES_AT_ONCE) ? 0 : 50);
-            } else {
-                let Module = (window as any).Module;
-                if (buf_len != 0) {
-                    Module._free(buf_addr);
-                }
-            }
-        }
-
-        checkXHR();
         update();
     }
 
