90d406faeb2fad1e4a7ffd7513c5395276605b21
kuviman <kuviman@gmail.com>
2017-09-04 18:48:31 +0300

    Make for debug

--- codewars2017/make-web-debug.bat
+++ codewars2017/make-web-debug.bat
@@ -5,9 +5,9 @@ pushd ..
 
 call emsdk\emsdk_env.bat
 set PATH=C:\Users\vkudasov\.cargo\bin;%NODEJS_HOME%;C:\Users\vkudasov\AppData\Roaming\npm;%PATH%
-set EMMAKEN_CFLAGS=-s TOTAL_MEMORY=268435456 -s FETCH=1 -s FETCH_DEBUG=1
+set EMMAKEN_CFLAGS=-s ALLOW_MEMORY_GROWTH=1 -s FETCH=1 -s FETCH_DEBUG=1
 popd
-cargo build --release --target=asmjs-unknown-emscripten
+cargo build --target=asmjs-unknown-emscripten
 if !errorlevel! neq 0 exit /b !errorlevel!
 pushd ..
 
@@ -15,17 +15,17 @@ set target_dir=target\web\asmjs\codewars2017
 mkdir %target_dir%
 
 for /d %%a in (
-    "target\asmjs-unknown-emscripten\release\build\codevisual_core_html-*"
+    "target\asmjs-unknown-emscripten\debug\build\codevisual_core_html-*"
 ) do copy /Y %%~fa\out\lib.html %target_dir%\codevisual.html
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 for /d %%a in (
-    "target\asmjs-unknown-emscripten\release\build\codevisual_core_css-*"
+    "target\asmjs-unknown-emscripten\debug\build\codevisual_core_css-*"
 ) do copy /Y %%~fa\out\lib.css %target_dir%\codevisual.css
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 for /d %%a in (
-    "target\asmjs-unknown-emscripten\release\build\codevisual_core_js-*"
+    "target\asmjs-unknown-emscripten\debug\build\codevisual_core_js-*"
 ) do copy /Y %%~fa\out\lib.js %target_dir%\codevisual.js
 if !errorlevel! neq 0 exit /b !errorlevel!
 
@@ -36,5 +36,5 @@ xcopy /E /Y /q src\core\web\lib %target_dir%\lib
 if !errorlevel! neq 0 exit /b !errorlevel!
 xcopy /E /Y /q codewars2017\static\* %target_dir%
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy /Y target\asmjs-unknown-emscripten\release\codewars2017.js %target_dir%\code.js
+copy /Y target\asmjs-unknown-emscripten\debug\codewars2017.js %target_dir%\code.js
 if !errorlevel! neq 0 exit /b !errorlevel!
