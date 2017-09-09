34d91f3689284f138631d38498c24b103900e40d
kuviman <kuviman@gmail.com>
2017-09-09 15:49:30 +0400

    Include CodeWars & Run to stats.sh

--- stats.sh
+++ stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find examples src "(" -name "*.rs" -o -name "*.ts" -o -name "*.less" -o -name "*.pug" -o -name "*.html" ")" "!" -name "index.d.ts" | xargs wc -l -w
+find codewars2017 run examples src "(" -name "*.rs" -o -name "*.ts" -o -name "*.less" -o -name "*.pug" -o -name "*.html" ")" "!" -name "index.d.ts" | xargs wc -l -w
