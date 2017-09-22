7e102f8a0c5151203c0a4c338dee64edcf04b2c6
kuviman <kuviman@gmail.com>
2017-09-23 02:17:20 +0300

    Do not leave framebuffer with only depth (fails on MacOS)

--- codewars2017/src/shadow_map/mod.rs
+++ codewars2017/src/shadow_map/mod.rs
@@ -2,7 +2,7 @@ use ::*;
 
 pub struct ShadowMap {
     app: Rc<codevisual::Application>,
-    map: Option<ugli::DepthTexture>,
+    map: Option<(ugli::Texture2d, ugli::DepthTexture)>,
     vehicle_material: Material,
     trees_material: Material,
 }
@@ -24,19 +24,22 @@ impl ShadowMap {
                                                 trees: &game_map::Trees,
                                                 framebuffer: &mut ugli::Framebuffer,
                                                 uniforms: U) -> &'a ugli::DepthTexture {
-        let need_size = framebuffer.get_size() * 2;
-        if self.map.as_ref().map_or(true, |map| map.get_size() != need_size) {
-            let mut map = ugli::DepthTexture::new_uninitialized(
-                self.app.ugli_context(), need_size);
-            map.set_filter(ugli::Filter::Nearest);
-            self.map = Some(map);
+        let need_size = framebuffer.get_size();
+        if self.map.as_ref().map_or(true, |map| map.0.get_size() != need_size) {
+            // TODO: need only depth, but fails on MacOS
+            self.map = Some((
+                ugli::Texture2d::new_uninitialized(
+                    self.app.ugli_context(), need_size),
+                ugli::DepthTexture::new_uninitialized(
+                    self.app.ugli_context(), need_size)));
         }
-        let texture = self.map.as_mut().unwrap();
+        let map = self.map.as_mut().unwrap();
+        let (color, depth) = (&mut map.0, &mut map.1);
         {
             let mut framebuffer = ugli::Framebuffer::new(
                 self.app.ugli_context(),
-                ugli::ColorAttachment::None,
-                ugli::DepthAttachment::Texture(texture));
+                ugli::ColorAttachment::Texture(color),
+                ugli::DepthAttachment::Texture(depth));
             let framebuffer = &mut framebuffer;
             ugli::clear(framebuffer, None, Some(1.0));
             for vehicles in vehicles.vehicles_by_type.values() {
@@ -68,6 +71,6 @@ impl ShadowMap {
                     ..Default::default()
                 });
         }
-        texture
+        depth
     }
 }
