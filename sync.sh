37f1ee44a17870c5398ab20b78609a59c54422ff
kuviman <kuviman@gmail.com>
2017-09-29 13:40:28 +0300

    Removed codewars & run from scripts

--- sync.sh
+++ sync.sh
@@ -6,8 +6,5 @@ set -e
 ./make-web.sh wasm32
 ./make-tutorial.sh asmjs
 ./make-tutorial.sh wasm32
-pushd codewars2017
-./make-web.sh
-popd
 # ./make-windows.sh
 rsync -avz --delete target/web/* pi@pi.kuviman.com:/home/pi/codevisual/
