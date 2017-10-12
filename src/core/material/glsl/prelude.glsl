b644a9fc9539448bfafd45b4d106dc30252205a4
kuviman <kuviman@gmail.com>
2017-10-12 17:55:03 +0300

    rotate(vec2, angle) in prelude.glsl

--- src/core/material/glsl/prelude.glsl
+++ src/core/material/glsl/prelude.glsl
@@ -6,6 +6,13 @@ precision highp float;
 
 uniform vec2 u_framebuffer_size;
 
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
+
 float G(vec2 pos, float s) {
     float sq2 = 2.0 * s * s;
     return exp(-dot(pos, pos) / sq2) / (PI * sq2);
