6c78eb4384dbf469d564d1e7445d4e49dedc932d
kuviman <kuviman@gmail.com>
2017-10-26 16:05:27 +0300

    Hide canvas scale setting

--- static_web/js/src/canvas.ts
+++ static_web/js/src/canvas.ts
@@ -5,17 +5,23 @@ namespace CodeVisual {
         export let canvas: HTMLCanvasElement;
 
         on_init.push(() => {
-            const scalingSetting = new NumberSetting("Canvas scale", 1, 4, 1, 0.01);
-            settings.add(scalingSetting);
             $canvas = $player.find("canvas");
             $canvas.on("contextmenu", () => false);
             canvas = $canvas[0] as HTMLCanvasElement;
             Module.canvas = canvas;
+
+            let scalingSettingValue = 1;
             setInterval(() => {
-                canvas.width = $canvas.width() / scalingSetting.value;
-                canvas.height = $canvas.height() / scalingSetting.value;
+                canvas.width = $canvas.width() / scalingSettingValue;
+                canvas.height = $canvas.height() / scalingSettingValue;
                 GLctx.viewport(0, 0, canvas.width, canvas.height);
             }, 300);
+
+            // const scalingSetting = new NumberSetting("Canvas scale", 1, 4, 1, 0.01);
+            // settings.add(scalingSetting);
+            // setInterval(() => {
+            //     scalingSettingValue = scalingSetting.value;
+            // }, 300);
         });
 
         export function set_cursor(cursor: string) {
