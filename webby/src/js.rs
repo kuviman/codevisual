c925addfdaef33bb441c56988b5854cfe165943a
kuviman <kuviman@gmail.com>
2017-11-26 17:48:00 +0400

    Unified Callback::into_json impls

--- webby/src/js.rs
+++ webby/src/js.rs
@@ -1,5 +1,9 @@
 use ::*;
 
+pub fn run_script(script: &str) {
+    emscripten::run_script(script);
+}
+
 proc_macro_expr_decl! {
     js! => js_impl
 }
@@ -28,70 +32,68 @@ impl<Args, F: StableFnMut<Args>> From<F> for Callback<Args, F> {
     }
 }
 
-impl<F: StableFnMut<(bool, )>> IntoJson for Callback<(bool, ), F> {
-    fn into_json(self) -> String {
-        let boxed = Box::new(self.f);
-        extern "C" fn wrapper<F: StableFnMut<(bool, )>>(f: c_int, b: c_int) {
-            let mut boxed = unsafe { Box::from_raw(f as *mut F) };
-            boxed.call_mut((b != 0, ));
-            mem::forget(boxed);
-        }
-        format!(
-            "function(b) {{ Runtime.dynCall('vii', {}, [{}, b ? 1 : 0]); }}",
-            (wrapper::<F> as c_int).into_json(),
-            (Box::into_raw(boxed) as c_int).into_json()
-        )
+pub trait CallbackArg {
+    const SIGNATURE: char;
+    type InteropType;
+    fn js_conversion(name: &str) -> String;
+    fn from(val: Self::InteropType) -> Self;
+}
+
+impl CallbackArg for i32 {
+    const SIGNATURE: char = 'i';
+    type InteropType = c_int;
+    fn js_conversion(name: &str) -> String {
+        name.to_owned()
+    }
+    fn from(val: c_int) -> i32 {
+        val as i32
     }
 }
 
-impl<F: StableFnMut<(i32, )>> IntoJson for Callback<(i32, ), F> {
-    fn into_json(self) -> String {
-        let boxed = Box::new(self.f);
-        extern "C" fn wrapper<F: StableFnMut<(i32, )>>(f: c_int, x: c_int) {
-            let mut boxed = unsafe { Box::from_raw(f as *mut F) };
-            boxed.call_mut((x as i32, ));
-            mem::forget(boxed);
-        }
-        format!(
-            "function(x) {{ Runtime.dynCall('vii', {}, [{}, x]); }}",
-            (wrapper::<F> as c_int).into_json(),
-            (Box::into_raw(boxed) as c_int).into_json()
-        )
+impl CallbackArg for bool {
+    const SIGNATURE: char = 'i';
+    type InteropType = c_int;
+    fn js_conversion(name: &str) -> String {
+        format!("{} == 0 ? 0 : 1", name)
+    }
+    fn from(val: c_int) -> bool {
+        val != 0
     }
 }
 
-impl<F: StableFnMut<(i32, i32)>> IntoJson for Callback<(i32, i32), F> {
-    fn into_json(self) -> String {
-        let boxed = Box::new(self.f);
-        extern "C" fn wrapper<F: StableFnMut<(i32, i32)>>(f: c_int, a: c_int, b: c_int) {
-            let mut boxed = unsafe { Box::from_raw(f as *mut F) };
-            boxed.call_mut((a as i32, b as i32));
-            mem::forget(boxed);
+macro_rules! impl_for_kind {
+    ($($name:ident),*) => {
+        impl<$($name:CallbackArg,)* F:StableFnMut<($($name,)*)>> IntoJson for Callback<($($name,)*),F> {
+            #[allow(warnings)]
+            fn into_json(self) -> String {
+                let boxed = Box::new(self.f);
+                extern "C" fn wrapper<$($name:CallbackArg,)* F:StableFnMut<($($name,)*)>>(f: c_int $(,$name:$name::InteropType)*) {
+                    let mut boxed = unsafe { Box::from_raw(f as *mut F) };
+                    boxed.call_mut(($($name::from($name),)*));
+                    mem::forget(boxed);
+                }
+                let mut signature = String::from("vi");
+                $(signature.push($name::SIGNATURE);)*
+                let mut js_conversions = String::new();
+                $(
+                    js_conversions.push(',');
+                    js_conversions += &$name::js_conversion(stringify!($name));
+                )*
+                format!(
+                    concat!("function(", $(stringify!($name),",",)* ") {{",
+                    "return Runtime.dynCall('{}', {}, [{}{}]);",
+                    "}}"),
+                    signature,
+                    (wrapper::<$($name,)* F> as c_int).into_json(),
+                    (Box::into_raw(boxed) as c_int).into_json(),
+                    js_conversions)
+            }
         }
-        format!(
-            "function(a, b) {{ Runtime.dynCall('viii', {}, [{}, a, b]); }}",
-            (wrapper::<F> as c_int).into_json(),
-            (Box::into_raw(boxed) as c_int).into_json()
-        )
     }
 }
 
-impl<F: StableFnMut<()>> IntoJson for Callback<(), F> {
-    fn into_json(self) -> String {
-        let boxed = Box::new(self.f);
-        extern "C" fn wrapper<F: StableFnMut<()>>(f: c_int) {
-            let mut boxed = unsafe { Box::from_raw(f as *mut F) };
-            boxed.call_mut(());
-            mem::forget(boxed);
-        }
-        format!(
-            "function(i) {{ Runtime.dynCall('vi', {}, [{}]); }}",
-            (wrapper::<F> as c_int).into_json(),
-            (Box::into_raw(boxed) as c_int).into_json()
-        )
-    }
-}
-
-pub fn run_script(script: &str) {
-    emscripten::run_script(script);
-}
+impl_for_kind!();
+impl_for_kind!(A);
+impl_for_kind!(A,B);
+impl_for_kind!(A,B,C);
+impl_for_kind!(A,B,C,D);
