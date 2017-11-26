9043a490681dd1c72f681d09cee8c721c821c031
kuviman <kuviman@gmail.com>
2017-11-27 00:46:57 +0400

    More predefined colors

--- prelude/src/color.rs
+++ prelude/src/color.rs
@@ -22,6 +22,14 @@ impl Color {
     pub const BLACK: Color = rgb!(0.0, 0.0, 0.0);
     pub const WHITE: Color = rgb!(1.0, 1.0, 1.0);
 
+    pub const RED: Color = rgb!(1.0, 0.0, 0.0);
+    pub const GREEN: Color = rgb!(0.0, 1.0, 0.0);
+    pub const BLUE: Color = rgb!(0.0, 0.0, 1.0);
+
+    pub const YELLOW: Color = rgb!(1.0, 1.0, 0.0);
+    pub const MAGENTA: Color = rgb!(1.0, 0.0, 1.0);
+    pub const CYAN: Color = rgb!(0.0, 1.0, 1.0);
+
     pub fn rgb(red: f32, green: f32, blue: f32) -> Self {
         Color {
             red,
