762ed8bd5cc3c4d8b15cd8ae65969693fcf9a7c9
kuviman <kuviman@gmail.com>
2017-12-01 05:23:03 +0400

    Removed Color::mix (need better API)

--- prelude/src/color.rs
+++ prelude/src/color.rs
@@ -55,16 +55,4 @@ impl Color {
             alpha: (value >> 24) as f32 / 255.0,
         }
     }
-
-    pub fn mix(color1: Self, color2: Self, k1: f32, k2: f32) -> Self {
-        let sum = k1 + k2;
-        let k1 = k1 / sum;
-        let k2 = k2 / sum;
-        Self {
-            red: color1.red * k1 + color2.red * k2,
-            green: color1.green * k1 + color2.green * k2,
-            blue: color1.blue * k1 + color2.blue * k2,
-            alpha: color1.alpha * k1 + color2.alpha * k2,
-        }
-    }
 }
