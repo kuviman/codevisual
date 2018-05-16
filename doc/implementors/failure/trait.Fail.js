2f175cfe58ae3b5c533deb958ddd198fa5ebb740
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-16 07:15:49 +0000

    Deploy codevisual.kuviman.com to github.com/kuviman/codevisual.git:gh-pages

--- doc/implementors/failure/trait.Fail.js
+++ doc/implementors/failure/trait.Fail.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["failure"] = [];
-implementors["ugli"] = ["impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"ugli/struct.ShaderCompilationError.html\" title=\"struct ugli::ShaderCompilationError\">ShaderCompilationError</a>","impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"ugli/struct.ProgramLinkError.html\" title=\"struct ugli::ProgramLinkError\">ProgramLinkError</a>",];
+implementors["ugli"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"ugli/struct.ShaderCompilationError.html\" title=\"struct ugli::ShaderCompilationError\">ShaderCompilationError</a>",synthetic:false,types:["ugli::shader::ShaderCompilationError"]},{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"ugli/struct.ProgramLinkError.html\" title=\"struct ugli::ProgramLinkError\">ProgramLinkError</a>",synthetic:false,types:["ugli::program::ProgramLinkError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/serde/de/trait.Error.js
+++ doc/implementors/serde/de/trait.Error.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
-implementors["serde_json"] = ["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>",];
-implementors["stdweb"] = ["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"stdweb/serde/struct.ConversionError.html\" title=\"struct stdweb::serde::ConversionError\">ConversionError</a>",];
+implementors["serde_json"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>",synthetic:false,types:["serde_json::error::Error"]},];
+implementors["stdweb"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"stdweb/serde/struct.ConversionError.html\" title=\"struct stdweb::serde::ConversionError\">ConversionError</a>",synthetic:false,types:["stdweb::ecosystem::serde::ConversionError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
