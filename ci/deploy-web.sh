2fd4757d90dc0d2b4efa4a6a1195a7ec1b94685c
kuviman <kuviman@gmail.com>
2018-05-24 01:59:44 +0400

    Fix doc deploy

--- ci/deploy-web.sh
+++ ci/deploy-web.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 cargo web deploy --target wasm32-unknown-emscripten --release --package codevisual-demo
 cargo doc --target wasm32-unknown-emscripten --release --package codevisual
-cp target/doc target/deploy
+cp -rf target/doc target/deploy
 echo '<meta http-equiv=refresh content=0;url=codevisual/index.html>' > target/deploy/doc/index.html
