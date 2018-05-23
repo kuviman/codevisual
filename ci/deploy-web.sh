df0cc1ebfd67f17104dddc19638a6e90802ca14e
kuviman <kuviman@gmail.com>
2018-05-24 01:10:26 +0400

    Use same config for doc as for web deploy

--- ci/deploy-web.sh
+++ ci/deploy-web.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cargo web deploy --release --package codevisual-demo
-cargo doc --package codevisual
+cargo web deploy --target wasm32-unknown-emscripten --release --package codevisual-demo
+cargo doc --target wasm32-unknown-emscripten --release --package codevisual
 cp target/doc target/deploy
 echo '<meta http-equiv=refresh content=0;url=codevisual/index.html>' > target/deploy/doc/index.html
